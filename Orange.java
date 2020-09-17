b12c4cf7f4bdf37643c587bd6efe608ca169bb21
Lukas BARTHELEMY <lb853849@MI115-02.iem>
2020-09-17 16:06:23 +0200

    premier commit

---
+++ Orange.java
@@ -0,0 +1,78 @@
+/**
+ *
+ * @author roudet
+ */
+public class Orange {
+    private double prix;
+    private String origine;
+	
+    public Orange() 
+    {
+        this.prix = 0.5;  //prix en euros
+        this.origine="Espagne";
+    }
+    
+    public Orange(double prix, String origine) 
+    {
+	if(prix < 0)
+	    this.prix = -prix;  //une solution possible pour interdire les prix negatifs
+	else
+	    this.prix = prix;
+
+	if(origine.equals(""))
+            this.origine = "Espagne";  //Espagne par défaut
+	else
+            this.origine = origine;   
+    }
+
+    public double getPrix(){
+	return prix;
+    }
+
+    public void setPrix(double prix){
+	this.prix=prix;
+    }
+
+    public String getOrigine(){
+	return origine;
+    }
+ 
+    public void setOrigine(String origine){
+	this.origine=origine;
+    }
+
+    @Override
+    public String toString(){
+        return "Orange de " + origine + " a " + prix + " euros";
+    }
+
+    @Override
+    public boolean equals(Object o){  //predicat pour tester si 2 oranges sont equivalentes
+        if(o != null && getClass() == o.getClass()){
+            Orange or = (Orange) o;
+            return (prix == or.prix && origine.equals(or.origine));
+        }
+        return false;
+    }
+
+    public boolean isSeedless() {  //predicat indiquant qu'une orange a des pepins
+        return false;
+    }
+
+
+    public static void main (String[] args){
+        //Ecrire ici vos tests
+	System.out.println("premier test Orange");
+	Orange o1 = new Orange();
+	if(o1.getPrix() == 0.5 && o1.getOrigine().equals("Espagne"))
+		System.out.println("orange 1 ok");
+
+	Orange o2 = new Orange(1.0, "France");
+	if(o2.getPrix() == 1.0 && o2.getOrigine() == "France")
+		System.out.println("orange 2 ok");
+	
+	Orange o3 = new Orange(-0.2, "Chine");
+	if(o3.getPrix() == 0.2 && o3.getOrigine() == "Chine")
+		System.out.println("orange 3 ok");
+   }
+}
